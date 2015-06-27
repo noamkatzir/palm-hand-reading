0d31288501e47306f13d4ff96b46643bbdc3376e
Noam Katzir <noam.katzir1@gmail.com>
2015-06-27 21:39:56 +0300

    refactor- using dict from now + comments

--- test6.py
+++ test6.py
@@ -4,11 +4,13 @@ import os
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+from numpy import linalg
+from operator import itemgetter, attrgetter, methodcaller
 
-#return list of center sorted descending to the contour area
-#and the min rect cordinates
-def sortObjects(contours):
-    areas = []
+# return list of center sorted descending to the contour area
+# and the min rect cordinates
+def mapPalmAndFingers(contours, image):
+    handElements = []
     for i in xrange(len(contours)):
         M = cv2.moments(contours[i])
         centroid_x = int(M['m10']/M['m00'])
@@ -16,52 +18,75 @@ def sortObjects(contours):
 
         # cv2.circle(lefthand, (centroid_x, centroid_y), 10, (255, 0, 0),-1)
 
-        areas.append([cv2.contourArea(contours[i]), i, centroid_x, centroid_y])
+        element = {
+            'center': (centroid_x, centroid_y),
+            'contour': contours[i],
+            'area': cv2.contourArea(contours[i]),
+            'palm': False
+        }
 
-    arr= np.array(areas)
-    return arr[arr[:, 0].argsort()[::-1]]
+        handElements.append(element)
+
+    # mapp the distance of hand elements from the palm
+    palm = max(handElements, key=lambda x: x['area'])
+    palm['palm'] = True
+    palm['dist'] = 0
+
+    for handElement in handElements:
+        if handElement['palm'] is False:
+            handElement['dist'] = linalg.norm(np.array(handElement['center']) - np.array(palm['center']))
+
+    # sorting the hand helements by the distance from the palm
+    # because this way we will find the end of the fingers
+    handElements = sorted(handElements, key=itemgetter('dist'), reverse=True)
+
+    # for handElement in handElements:
+    #     cv2.fitLine(handElement['contour'])
+
+    return handElements, palm
 
 # lefthand = cv2.imread('noam_left_hand_30.5.15_02062015_0001.png')
 lefthand = cv2.imread('noam_left_hand_6.12.08_02062015.png')
 
 lefthand_imgray = cv2.cvtColor(lefthand,cv2.COLOR_BGR2GRAY)
 
+# I resize the image to very samll size and then get the samll image back to the size of the origin
+# this way I remove most of the noise and keep the large objects
 small = cv2.resize(lefthand_imgray, (0, 0), fx=0.5**6, fy=0.5**6)
-
 small = cv2.resize(small, (lefthand_imgray.shape[1], lefthand.shape[0]),interpolation=cv2.INTER_CUBIC)
+
+# I put the threshold to get the hand elements binay image
 ret, threshold1 = cv2.threshold(small,245,255,cv2.THRESH_BINARY_INV)
 contours, hierarchy = cv2.findContours(threshold1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 mask = np.zeros(threshold1.shape,np.uint8)
 
-
-
+# I'm filling the contours of with large area to ensure I get a the objects with minimum noise
 for i in xrange(len(contours)):
     area = cv2.contourArea(contours[i])
-    if(area > 70000): #in origin it was 300000, but it start ignoring fingers
+
+    # in origin it was 300000, but it start ignoring fingers
+    if area > 70000:
         cv2.drawContours(mask, contours,i,255,-1)
 
+# converting again to binary image & finding the contours of the large objects
 ret, threshold2 = cv2.threshold(mask, 0, 256, cv2.THRESH_BINARY)
-contours2, hierarchy2 = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+contours2, hierarchy2 = cv2.findContours(mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 mask = mask.astype('uint8')
-cv2.drawContours(lefthand,contours2,-1,(0,255,0),3)
-
-objects = sortObjects(contours2)
-
-center = np.array([objects[0][2], objects[0][3]])
+cv2.drawContours(lefthand, contours2, -1, (0, 255, 0), 3)
 
 
-for i in xrange(1,len(objects)):
-    dist = np.linalg.norm(np.array([objects[i][2], objects[i][3]]) - center)
-    # if dist > 2500:
-    #     continue
+elements, palm = mapPalmAndFingers(contours2, lefthand)
 
-    cv2.line(lefthand, (int(objects[0][2]), int(objects[0][3])), (int(objects[i][2]), int(objects[i][3])), (0,255,0),5)
-    cv2.putText(lefthand,'{}'.format(dist),(int(objects[i][2]), int(objects[i][3])), cv2.FONT_HERSHEY_SIMPLEX, 4, 1,20)
-    cnt = contours2[int(objects[i][1])]
-    ellipse = cv2.fitEllipse(cnt)
-    cv2.ellipse(lefthand,ellipse,(255,0,0),2)
-    print('distance between center and {} is {}'.format(i, dist))
-    print('the center is ({},{}) and the ellipse center({},{}), height/width({},{}), angle {}'.format(int(objects[i][2]), int(objects[i][3]), ellipse[0][0], ellipse[0][1], ellipse[1][0], ellipse[1][1], ellipse[2]))
+for element in elements:
+
+    cv2.line(lefthand, palm['center'], element['center'], (0, 255, 0), 5)
+    ellipse = cv2.fitEllipse(element['contour'])
+    cv2.ellipse(lefthand, ellipse, (255, 0, 0), 2)
+    #cv2.putText(lefthand, '{}'.format(dist), element['center'], cv2.FONT_HERSHEY_SIMPLEX, 4, 1, 20)
+
+# largeCon = np.concatenate((elements[0]['contour'], elements[1]['contour']), axis=0)
+# ellipse = cv2.fitEllipse(largeCon)
+# cv2.ellipse(lefthand, ellipse, (255, 0, 0), 2)
 
 plt.figure(1)
 plt.imshow(mask, cmap='gray')
