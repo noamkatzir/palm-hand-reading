fix the fingers rotation calculation and the final normalization of the angle

FindHand.py
@@ -152,6 +152,13 @@ class FindHand:
         angle = np.pi / 2
         if xdiff is not 0:
             angle = math.atan(ydiff / xdiff)
+
+        if xdiff < 0:
+            if ydiff > 0:
+                angle += np.pi
+            elif ydiff < 0:
+                angle -= np.pi
+
         angle = (angle / np.pi) * 180
         return angle
 
@@ -167,6 +174,7 @@ class FindHand:
         rotatedContour = np.dot(M, homogeneous_center)
         self.palm['rotatedCenter'] = rotatedContour[0],rotatedContour[1]
 
+        self.rotatedContours = []
         for handElement in self.handElements:
             homogeneous_representation = np.ones((handElement['contour'].shape[0], 3), np.uint8)
             contour = handElement['contour'].copy().reshape((handElement['contour'].shape[0],2))
@@ -204,7 +212,7 @@ class FindHand:
         center = np.array(self.palm['rotatedCenter'])
 
         for finger in self.fingers:
-            pos = center - np.array(finger[0]['rotatedCenter'])
+            pos = np.array(finger[0]['rotatedCenter']) - center
 
             finger[0]['angleFromPalmCenter'] = self._calculate_angle(pos[0], pos[1])
 

main.py
@@ -14,6 +14,7 @@ first_hand.find_hand_elements()
 print len(first_hand.fingers)
 
 small1 = first_hand.small_image.copy()
+small1_orig = first_hand.small_image.copy()
 
 # for finger in first_hand.fingers:
 #     finger_merged_contour = np.concatenate([element['contour'] for element in finger])
@@ -29,7 +30,10 @@ first_hand.calculate_normalization_box_angle()
 first_hand.rotate_contours_according_palm_center(first_hand.palm['normalization_angle'])
 first_hand.map_fingers_and_orientation()
 first_hand.map_fingers_angles_from_palm()
-small1 = first_hand.rotate_image(small1, first_hand.palm['normalization_angle'])
+full_normalization_angle1 = first_hand.palm['normalization_angle'] + first_hand.orientation_angle + (first_hand.fingers[-3][0]['angleFromPalmCenter'] - first_hand.orientation_angle)
+first_hand.rotate_contours_according_palm_center(full_normalization_angle1)
+small1_normalized = first_hand.rotate_image(small1, first_hand.palm['normalization_angle'])
+small1 = first_hand.rotate_image(small1, full_normalization_angle1)
 cv2.circle(small1, (int(first_hand.palm['rotatedCenter'][0]), int(first_hand.palm['rotatedCenter'][1])), 3, (0, 0, 255), -1)
 
 for i in xrange(len(first_hand.rotatedContours)):
@@ -40,8 +44,8 @@ for i in xrange(len(first_hand.fingers)):
 
 # small1 = ndimage.rotate(small1, first_hand.get_normalization_box_angle())
 
-# second_hand = fh.FindHand('./images/preprocessed/noam_left_hand_30.5.15_02062015_0001.png', 'left')
-second_hand = fh.FindHand('./images/preprocessed/noam_right_hand_30.5.15_02062015.png', 'right')
+second_hand = fh.FindHand('./images/preprocessed/noam_left_hand_30.5.15_02062015_0001.png', 'left')
+# second_hand = fh.FindHand('./images/preprocessed/noam_right_hand_30.5.15_02062015.png', 'right')
 
 second_hand.create_map_with_pyramid()
 second_hand.find_hand_elements()
@@ -49,6 +53,7 @@ second_hand.find_hand_elements()
 print len(second_hand.fingers)
 
 small2 = second_hand.small_image.copy()
+small2_orig = second_hand.small_image.copy()
 
 # for finger in second_hand.fingers:
 #     finger_merged_contour = np.concatenate([element['contour'] for element in finger])
@@ -63,7 +68,10 @@ second_hand.calculate_normalization_box_angle()
 second_hand.rotate_contours_according_palm_center(second_hand.palm['normalization_angle'])
 second_hand.map_fingers_and_orientation()
 second_hand.map_fingers_angles_from_palm()
-small2 = second_hand.rotate_image(small2, second_hand.palm['normalization_angle'])
+full_normalization_angle2 = second_hand.palm['normalization_angle'] + second_hand.orientation_angle + (second_hand.fingers[-3][0]['angleFromPalmCenter'] - second_hand.orientation_angle)
+second_hand.rotate_contours_according_palm_center(full_normalization_angle2)
+small2_normalized = second_hand.rotate_image(small2, second_hand.palm['normalization_angle'])
+small2 = second_hand.rotate_image(small2, full_normalization_angle2)
 cv2.circle(small2, (int(second_hand.palm['rotatedCenter'][0]), int(second_hand.palm['rotatedCenter'][1])), 3, (0, 0, 255), -1)
 
 for i in xrange(len(second_hand.rotatedContours)):
@@ -77,9 +85,21 @@ for i in xrange(len(second_hand.fingers)):
 
 plt.figure(1)
 plt.subplot(231)
+plt.imshow(small1_orig, cmap='gray')
+plt.title('2008 origin')
+plt.subplot(232)
+plt.imshow(small1_normalized, cmap='gray')
+plt.title('2008 normalized')
+plt.subplot(233)
 plt.imshow(small1, cmap='gray')
 plt.title('2008')
-plt.subplot(232)
+plt.subplot(234)
+plt.imshow(small2_orig, cmap='gray')
+plt.title('2015 origin')
+plt.subplot(235)
+plt.imshow(small2_normalized, cmap='gray')
+plt.title('2015 normalized')
+plt.subplot(236)
 plt.imshow(small2, cmap='gray')
 plt.title('2015')
 plt.show()